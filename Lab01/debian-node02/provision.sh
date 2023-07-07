19bbddda117a737abec4d411f90555d8c74c44e8
Renato Miranda Setubal <remiset@gmail.com>
2023-07-06 22:05:11 -0300

    Nova máquina para testar roles Debian-Node02

--- Lab01/debian-node02/provision.sh
+++ Lab01/debian-node02/provision.sh
@@ -4,7 +4,7 @@ apt install ansible -y
 apt install htop -y
 apt install net-tools -y
 cat  <<EOT >> /etc/hosts
-    192.168.0.80 debian-node
+    192.168.0.90 Debian-Node02
     192.168.0.81 bd01
     192.168.0.82 ap01
 EOT
